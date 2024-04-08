c3e9365eae875c301de21f8d1f19ad8fc7284077
yithril <yithril@gmail.com>
2024-04-08 19:45:39 -0400

    class example

---
+++ Week1/1.Monday/DataInJava/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //variables and data
+        //data that is letters or letters and numbers is a string
+        String emailAddress = "superDad1234@hallmark.com";
+
+        //whole number no fractions!!!!
+        int numberOfEmployees = 30;
+        long numberOfGalaxies = 1000000000;
+
+        //hourly rate
+        float hourlyRate = 15.75f;
+        double bankBalance = 1000.65;
+
+        //pass or fail
+        boolean hasPassed = true;
+
+        //char, byte, short
+        char letter = 'A';
+    }
+}
