ba900af558c3d628fe2ba8510d3afcb72d8661e2
Jonathan Hop <yithril@gmail.com>
2024-06-20 08:55:25 -0400

    warmup

--- Week11/1.Monday/our-first-api/src/main/java/com/example/our_first_api/models/Recipe.java
+++ Week11/1.Monday/our-first-api/src/main/java/com/example/our_first_api/models/Recipe.java
@@ -1,14 +1,12 @@
 package com.example.our_first_api.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -16,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
